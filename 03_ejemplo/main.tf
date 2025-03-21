9838c466c69223292bc3b17ba8957bd6c63af7dc
Piterxus <piterxus@gmail.com>
2025-03-21 19:17:15 +0100

    chore: Add new Terraform exercise

---
+++ 03_ejemplo/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source              = "./modulos/vpc"
+  vpc_cidr            = "10.0.0.0/16"
+  vpc_name            = "MiVPC"
+  subnet_publica_cidr = "10.0.1.0/24"
+  subnet_privada_cidr = "10.0.2.0/24"
+}
+module "ec2" {
+  source        = "./modulos/ec2"
+  ami_id        = "ami-0ac4dfaf1c5c0cce9"
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.subnet_publica_id
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+  
+}
+
+output "ec2_id" {
+  value = module.ec2.ec2_id
+  
+}
