913756aa61fe06a594805a65b1bee30e439eb617
piterxus <piterxus@gmail.com>
2025-04-22 17:46:21 +0200

    feat: Add security group for instances

--- 03_ejemplo/main.tf
+++ 03_ejemplo/main.tf
@@ -5,11 +5,18 @@ module "vpc" {
   subnet_publica_cidr = "10.0.1.0/24"
   subnet_privada_cidr = "10.0.2.0/24"
 }
+
+module "security_group_instance" {
+  source              = "./modulos/security_group_instance"
+  vpc_id              = module.vpc.vpc_id
+  security_group_name = "instances-sg"
+}
 module "ec2" {
   source        = "./modulos/ec2"
   ami_id        = "ami-0e449927258d45bc4"
   instance_type = "t2.micro"
   subnet_id     = module.vpc.subnet_publica_id
+  security_group_id  = module.security_group_instance.security_group_id
 }
 
 module "ec2_ubuntu" {
@@ -17,6 +24,7 @@ module "ec2_ubuntu" {
   ami_id        = "ami-084568db4383264d4"
   instance_type = "t3.micro"
   subnet_id     = module.vpc.subnet_publica_id
+  security_group_id  = module.security_group_instance.security_group_id
 }
 
 module "ec2_windows" {
@@ -24,6 +32,7 @@ module "ec2_windows" {
   ami_id        = "ami-02e3d076cbd5c28fa"
   instance_type = "t3.large"
   subnet_id     = module.vpc.subnet_publica_id
+  security_group_id  = module.security_group_instance.security_group_id
 }
 
 module "bucket" {
--- 03_ejemplo/modulos/ec2Windows/variables.tf
+++ 03_ejemplo/modulos/ec2Windows/variables.tf
@@ -1,3 +1,4 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "subnet_id" {}
+variable "security_group_id" {}
